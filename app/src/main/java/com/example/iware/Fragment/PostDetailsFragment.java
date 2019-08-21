42ef10a5066c063c1732341a9e539332cedd94b7
xLax <30602376+xLax@users.noreply.github.com>
2019-08-22 00:34:00 +0300

    finished delete button

--- app/src/main/java/com/example/iware/Fragment/PostDetailsFragment.java
+++ app/src/main/java/com/example/iware/Fragment/PostDetailsFragment.java
@@ -1,6 +1,8 @@
 package com.example.iware.Fragment;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
@@ -13,10 +15,15 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.iware.Adapter.PostAdapter;
+import com.example.iware.MainActivity;
 import com.example.iware.Model.Post;
+import com.example.iware.PostActivity;
 import com.example.iware.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -33,6 +40,9 @@ public class PostDetailsFragment extends Fragment
     private RecyclerView recyclerView;
     private PostAdapter postAdapter;
     private List<Post> postList;
+
+    ImageView deletePost;
+    String postPublisher;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -52,9 +62,56 @@ public class PostDetailsFragment extends Fragment
         postAdapter = new PostAdapter(getContext(), postList);
         recyclerView.setAdapter(postAdapter);
 
+        deletePost = view.findViewById(R.id.delete_post);
+
         readPosts();
 
+
+
         return view;
+    }
+
+    private void manageDeleteButton(final Post currentPost)
+    {
+        final ProgressDialog progressDialog = new ProgressDialog(getContext());
+        SharedPreferences prefs = getContext().getSharedPreferences("PREFS", Context.MODE_PRIVATE);
+
+        final String profileid = prefs.getString("profileid", "none");
+        postPublisher = currentPost.getPublisher();
+
+        if (profileid.equals(postPublisher))
+        {
+            deletePost.setVisibility(View.VISIBLE);
+            deletePost.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    progressDialog.setMessage("Deleting");
+                    progressDialog.show();
+
+                    String postid = currentPost.getPostid();
+
+                    DatabaseReference reference;
+                    reference = FirebaseDatabase.getInstance().getReference("Posts");
+                    reference.child(postid).removeValue();
+
+                    reference = FirebaseDatabase.getInstance().getReference("Comments");
+                    reference.child(postid).removeValue();
+
+                    reference = FirebaseDatabase.getInstance().getReference("Likes");
+                    reference.child(postid).removeValue();
+
+                    progressDialog.dismiss();
+
+                    startActivity(new Intent(getContext(), MainActivity.class));
+
+                }
+            });
+        }
+        else
+        {
+            deletePost.setVisibility(View.GONE);
+        }
     }
 
     private void readPosts()
@@ -67,9 +124,12 @@ public class PostDetailsFragment extends Fragment
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 postList.clear();
                 Post post = dataSnapshot.getValue(Post.class);
-                postList.add(post);
-
-                postAdapter.notifyDataSetChanged();
+                if (post != null)
+                {
+                    postList.add(post);
+                    postAdapter.notifyDataSetChanged();
+                    manageDeleteButton(post);
+                }
             }
 
             @Override
